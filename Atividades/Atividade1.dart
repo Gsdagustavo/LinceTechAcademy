e5cda064917bd7d0c10de2458802c14d1b980eb9
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-18 07:48:21 -0300

    implementada classe Input com metodos para entrada de dados e adicionados testes no main (Atividade 1)

--- Atividades/Atividade1.dart
+++ Atividades/Atividade1.dart
@@ -1,3 +1,21 @@
+import 'dart:io';
+
+import '../Util/Input.dart';
+
 void main() {
 
+  // testar a funcao pegar inputs de strings
+  stdout.write('string: ');
+  String name = Input.getStringInput();
+  print(name);
+
+  // testar a funcao para pegar inputs de inteiros
+  stdout.write('\nint: ');
+  int num = Input.getIntInput();
+  print(num);
+
+  // testar a funcao para pegar inputs de doubles
+  stdout.write('\ndouble: ');
+  double dNum = Input.getDoubleInput();
+  print(dNum);
 }
