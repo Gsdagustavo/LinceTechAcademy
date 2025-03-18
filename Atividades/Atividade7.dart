6bc1fa6a3ed5ac8f1073eb9d5752ce9eb72b15d4
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-18 08:57:20 -0300

    feita a atividade 7

--- Atividades/Atividade7.dart
+++ Atividades/Atividade7.dart
@@ -1,3 +1,34 @@
-void main() {
+import 'dart:io';
 
+import '../Util/Input.dart';
+
+void main() {
+  stdout.write('Insira um numero e calcularei seu fatorial: ');
+  int n = Input.getIntInput();
+
+  int fatorial = calcularFatorial(n);
+
+  if (fatorial != 0) {
+    print('Fatorial de $n: $fatorial');
+  }
+}
+
+int calcularFatorial(int n) {
+
+  if (n == 1 || n == 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    print('Valor invalido');
+    return 0;
+  }
+
+  int resultado = n;
+
+  for (int i = n - 1; i > 1; i--) {
+    resultado *= i;
+  }
+
+  return resultado;
 }
