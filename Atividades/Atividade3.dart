8ca8d19a24473904c01022798a09b339dfce8ab1
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-18 07:58:55 -0300

    feita a atividade 3

--- Atividades/Atividade3.dart
+++ Atividades/Atividade3.dart
@@ -1,3 +1,17 @@
+import 'dart:io';
+
+import '../Util/Input.dart';
+
 void main() {
 
+  // pede ao usuario para inserir os numeros
+  stdout.write('Insira o primeiro numero: ');
+  double n1 = Input.getDoubleInput();
+
+  stdout.write('Insira o segundo numero: ');
+  double n2 = Input.getDoubleInput();
+
+  double resultado = (n1 - n2).abs();
+
+  print('Resultado de $n1 - $n2 em modulo: ${resultado}');
 }
