124f0b1cbc321151ec259d8720923cd6d47c07c8
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-18 08:59:36 -0300

    feita a atividade 8

--- Atividades/Atividade8.dart
+++ Atividades/Atividade8.dart
@@ -1,3 +1,16 @@
-void main() {
+import 'dart:io';
 
+import '../Util/Input.dart';
+
+void main() {
+  stdout.write('Insira um numero e mostrarei sua tabuada: ');
+  int n = Input.getIntInput();
+
+  mostrarTabuada(n);
+}
+
+void mostrarTabuada(int n) {
+  for (int i = 1; i <= 9; i++) {
+    print('$n X $i = ${n * i}');
+  }
 }
