b2e7661f58b7523b41b9fad6dbc1cff6bb8916dc
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-18 07:57:10 -0300

    feita a atividade 2

--- Atividades/Atividade2.dart
+++ Atividades/Atividade2.dart
@@ -1,3 +1,28 @@
+import 'dart:io';
+
+import '../Util/Input.dart';
+
 void main() {
 
+  // constante para armazenar o tamanho maximo da lista
+  const int numsSize = 5;
+  List<int> numeros = [];
+
+  // um for que pede o input do usuario 5 vezes e armazena cada input na lista
+  for (int i = 0; i < numsSize; i++) {
+    stdout.write('Insira o ${i + 1} numero: ');
+    int num = Input.getIntInput();
+    numeros.add(num);
+  }
+
+  print('\nMostrando todos os numeros impares\n');
+
+  // loop que itera por todos os elementos
+  for (int i = 0; i < numsSize; i++) {
+
+    // se o numero for impar, mostre ao usuario, indicando sua posicao na lista
+    if (numeros[i] % 2 != 0) {
+      print('${i + 1}. ${numeros[i]}');
+    }
+  }
 }
