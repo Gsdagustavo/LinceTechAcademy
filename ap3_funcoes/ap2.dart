3555c4e0e42cc63ee936e66d0c5dc55b9a79629f
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-25 18:08:03 -0300

    testes para resolver a atividade 2

--- ap3_funcoes/ap2.dart
+++ ap3_funcoes/ap2.dart
@@ -1,3 +1,14 @@
 void main() {
-   
+  funcaoA(funcaoB);
+}
+
+void funcaoA(void outraFuncao()) {
+  print('funcao a');
+
+  print('chamando a funcao B');
+  outraFuncao();
+}
+
+void funcaoB() {
+  print('funcao b');
 }
