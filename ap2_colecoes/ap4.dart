d06a788a1ff1cbbfc7c8e56ebc5e5eef659a515b
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-25 14:37:41 -0300

    refiz a implementacao da ultima parte da atividade 4

--- ap2_colecoes/ap4.dart
+++ ap2_colecoes/ap4.dart
@@ -1,7 +1,7 @@
 void main() {
 
   // cria o mapa com a sigla dos estados e suas cidades
-  Map<String, List<String>> mapaCidades = {
+  final Map<String, List<String>> mapaCidades = {
     'MG': ['Belo Horizonte', 'Juiz de Fora', 'Berlinda'],
     'PR': ['Cascavel', 'Curitiba', 'Foz do Iguaçu'],
     'SC': ['Blumenau', 'Florianopolis', 'Gaspar'],
@@ -15,7 +15,7 @@ void main() {
 
   // mostra as cidades de SC em ordem alfabetica usando o metodo .sort()
   if (mapaCidades.containsKey('SC')) {
-    List<String> cidadesSC = mapaCidades['SC']!;
+    final List<String> cidadesSC = mapaCidades['SC']!;
     cidadesSC.sort();
     print('Cidades de SC: ${cidadesSC.join(' ; ')}');
   }
@@ -23,13 +23,24 @@ void main() {
   print('');
 
   // cria uma lista e adiciona todas as cidades do mapa de cidades com suas siglas nela
-  List<String> todasCidades = [];
+  final List<String> todasCidades = [];
 
-  mapaCidades.forEach((sigla, cidades) {
-    cidades.forEach((cidade) {
-      todasCidades.add('$cidade - $sigla');
-    });
-  });
+  /* metodo que fiz para adicionar todas as cidades na lista, com as siglas dos estados
+    entretanto, ficou meio confuso, entao refiz usando for each normal (sem metodos)
+ */
+  // mapaCidades.forEach((sigla, cidades) {
+  //   cidades.forEach((cidade) {
+  //     todasCidades.add('$cidade - $sigla');
+  //   });
+  // });
+
+  // metodo para adicionar as cidades à lista
+  for (String estado in mapaCidades.keys) {
+
+    for (String cidade in mapaCidades[estado]!) {
+      todasCidades.add('$cidade - $estado');
+    }
+  }
 
   // mostra todas as cidades em ordem alfabetica
   for (String cidade in todasCidades) {
