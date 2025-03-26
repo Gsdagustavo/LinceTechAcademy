566715d510b4b28c906adb46c3a7f8703ae32fe1
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-26 05:40:38 -0300

    resolvida a atividade 3

--- ap4_operadores/ap3.dart
+++ ap4_operadores/ap3.dart
@@ -1,3 +1,11 @@
 void main() {
+  final List<int> lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  removerElemento(lista, );
+
+  print('Lista: ${lista.join(', ')}');
+}
+
+List removerElemento(List lista, [dynamic elemento]) {
+  return (lista ?? [])..remove(elemento);
 }
