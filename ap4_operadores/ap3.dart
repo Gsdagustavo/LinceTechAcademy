b4305c8545bdff22ee2f170a7232b97366b16c45
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-27 10:29:47 -0300

    refeita a atividade 3

--- ap4_operadores/ap3.dart
+++ ap4_operadores/ap3.dart
@@ -1,11 +1,33 @@
 void main() {
-  final List<int> lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  final List<dynamic> lista = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    'Joao', 'Pedro'
+  ];
 
-  removerElemento(lista, );
+  // mostrar a lista antes de realiza as operacoes
+  print('Lista: ${lista.join(', ')}\n');
 
-  print('Lista: ${lista.join(', ')}');
+  /* realizar operacoes com a lista */
+  removerElemento(lista: lista, elemento: 5);
+  removerElemento(lista: lista, elemento: 1);
+
+  removerElemento(lista: lista, elemento: 'Joao');
+
+  removerElemento(lista: lista, elemento: '123');
+
+  // mostrar a lista apos as operacoes
+  print('\nLista: ${lista.join(', ')}');
 }
 
-List removerElemento(List lista, [dynamic elemento]) {
-  return (lista ?? [])..remove(elemento);
+/**
+ * Funcao que recebe uma lista e um elemento como argumento e retorna a lista
+ * com o elemento removido caso a operacao de remover tenha dado certo.
+ * Caso contrario, retorna uma lista vazia
+ */
+List removerElemento({required List? lista, required dynamic elemento}) {
+  bool removido = lista?.remove(elemento) ?? false;
+
+  print('O elemento $elemento ${removido ? 'foi' : 'nao foi'} removido da lista');
+
+  return lista ?? [];
 }
