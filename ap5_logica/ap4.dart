7866189690869b725079e4f787f2b89833c455d9
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-27 11:31:02 -0300

    resolvida a atividade 4

--- ap5_logica/ap4.dart
+++ ap5_logica/ap4.dart
@@ -1,3 +1,40 @@
 void main() {
+  final listaNomes = [
+    "Joao",
+    "Maria",
+    "Pedro",
+    "Maria",
+    "Ana",
+    "Joao",
+    "Maria",
+    "Fernanda",
+    "Carlos",
+    "Maria"
+  ];
 
+  final nome = 'Ana';
+  final quantidade = contarNome(listaNomes, nome);
+
+  if (quantidade == 1) {
+    print('O nome $nome foi encontrado 1 vez');
+  } else if (quantidade > 0) {
+    print('O nome $nome foi encontrado $quantidade vezes');
+  } else {
+    print('O nome nao foi encontrado');
+  }
+
+  print(listaNomes);
+}
+
+/// Retorna a quantidade de vezes que o elemento [nome] foi encontrado na lista [listaNomes]
+///
+/// Eu criei uma lista igual à lista [listaNomes] e usei o metodo removeWhere na nova lista
+/// para remover todos os nomes que nao forem iguais ao [nome], e retornei o tamanho da nova lista
+/// após
+int contarNome(List<String> listaNomes, String nome) {
+  List<String> novaLista = listaNomes.toList();
+
+  novaLista.removeWhere((elemento) => elemento != nome);
+
+  return novaLista.length;
 }
