47974af6e494659f3b45b4fc26e9cbae32ff3163
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-27 11:37:30 -0300

    refeito o switch-case na questao 5

--- ap5_logica/ap5.dart
+++ ap5_logica/ap5.dart
@@ -5,20 +5,11 @@ void main() {
   final opcao = random.nextInt(6);
 
   switch (opcao) {
-    case 0:
-      print('Opcao invalida');
-      break;
     case 1:
-      print('Encontrado 1');
-      break;
     case 2:
-      print('Encontrado 2');
-      break;
     case 3:
-      print('Encontrado 3');
-      break;
     case 4:
-      print('Encontrado 4');
+      print('Encontrado $opcao');
       break;
     case 5:
       print('Encontrado final');
