6e3b8311e7270c8c79d33fd2f4fd612ec3907ae1
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-27 10:58:25 -0300

    resolvida a atividade 1

--- ap5_logica/ap1.dart
+++ ap5_logica/ap1.dart
@@ -1,3 +1,25 @@
+import 'dart:io';
+
 void main() {
 
+  // pede ao usuario para digitar um numero
+  stdout.write('Insira o numero inicial: ');
+  final int? numeroInicial = int.tryParse(stdin.readLineSync() ?? '');
+
+  if (numeroInicial == null || numeroInicial <= 0) {
+    print('Numero invalido. Insira apenas numeros positivos diferentes de zero');
+    return;
+  }
+
+  imprimirNumeros(numeroInicial: numeroInicial);
+}
+
+/// Imprime todos os numeros impares de 1 ate o numero fornecido (inclusivo)
+void imprimirNumeros({required int numeroInicial}) {
+
+  for (int i = 1; i <= numeroInicial; i++) {
+    if (i.isOdd) {
+      print('Impar: $i');
+    }
+  }
 }
