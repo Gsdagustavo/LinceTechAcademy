e332ec3e1267ced2a2cee015cdef632d4dcf5d22
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-03-30 10:36:40 -0300

    resolvida a atividade 1

--- ap6_erros/ap1.dart
+++ ap6_erros/ap1.dart
@@ -1,3 +1,23 @@
-void main() {
+import 'dart:io';
 
+void main(List<String> args) {
+
+  stdout.write('Digite um numero: ');
+  String input = stdin.readLineSync() ?? '';
+
+  int? num = converterStringParaInt(string: input);
+
+  if (num != null) {
+    print('Numero digitado: $num');
+  }
+}
+
+int? converterStringParaInt({required String string}) {
+  
+  try {
+    return int.parse(string);
+  } catch (e) {
+    print('Entrada invalida. Digite apenas números inteiros');
+    return null;
+  }
 }
