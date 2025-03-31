91038288825973b6be421bc1bd52f1fe898469bc
Gsdagustavo <gugadanielalvez@gmail.com>
2025-03-31 17:55:43 -0300

    resolvida a atividade 4

--- ap6_erros/ap4.dart
+++ ap6_erros/ap4.dart
@@ -1,3 +1,32 @@
 void main() {
+  try {
+    final String nome = 'teste.txt';
 
+    ArquivoTexto arquivoTexto = ArquivoTexto(nome);
+    arquivoTexto.abrir();
+  } catch (e) {
+    print('Entrada invalida, digite um nome valido');
+    rethrow;
+  } finally {
+    print('Fim');
+  }
+}
+
+abstract class Arquivo {
+  void abrir();
+}
+
+class ArquivoTexto implements Arquivo {
+  late String _nome;
+
+  ArquivoTexto(this._nome);
+
+  @override
+  void abrir() {
+    try {
+      print('Abrindo o arquivo $_nome...');
+    } catch (e) {
+      throw Exception('Erro ao abrir o arquivo $_nome');
+    }
+  }
 }
