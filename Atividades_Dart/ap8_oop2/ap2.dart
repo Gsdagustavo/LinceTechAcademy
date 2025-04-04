b07fddb8823c5634d678a94a93c6cef2d87e9005
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-04-04 04:38:44 -0300

    resolvida a atividade 2 de oop2

--- Atividades_Dart/ap8_oop2/ap2.dart
+++ Atividades_Dart/ap8_oop2/ap2.dart
@@ -1,3 +1,29 @@
-void main() {
 
+/// Testar a implementação da classe [Gato]
+void main() {
+  Gato gato = Gato();
+
+  gato.comer();
+  gato.beber();
+  gato.miar();
+}
+
+/// Classe que possui dois méotos simulando o comportamento de uma animal comum
+abstract class Animal {
+
+  void comer() {
+    print('O animal está comendo');
+  }
+
+  void beber() {
+    print('O animal está bebendo');
+  }
+}
+
+/// Classe que estende a classe abstrata [Animal] e possui um método próprio ([miar])
+class Gato extends Animal {
+
+  void miar() {
+    print('O gato está miando');
+  }
 }
