edf5e174600ec903593f4e483c4f4bc1bb535d41
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-04-04 04:59:31 -0300

    resolvida a atividade 4 de oop2

--- Atividades_Dart/ap8_oop2/ap4.dart
+++ Atividades_Dart/ap8_oop2/ap4.dart
@@ -1,3 +1,27 @@
-void main() {
 
+/// Testar a implementação da classe [Calculadora]
+void main(List<String> args) {
+
+  // caso o usuário não passe nenhum argumento para a função main, o programa é encerrado
+  if (args.isEmpty) {
+    print('Entrada invalida');
+    return;
+  }
+  
+  try {
+      
+    double numero = double.parse(args[0]);
+    print('O dobro de $numero é ${Calculadora.dobro(numero)}');
+
+  } on FormatException catch (e) {
+    print('$e: Entrada invalida');
+  } on Exception catch (e, s) {
+    print('Um erro inesperado ocorreu: $e\nStackTrace: $s');
+  }
+}
+
+/// Classe abstrata (não pode ser instanciada) que possui um método estático ([dobro])
+/// que retorna o dobro do número recebido como argumento
+abstract class Calculadora {
+  static double dobro(double numero) => numero * 2;
 }
