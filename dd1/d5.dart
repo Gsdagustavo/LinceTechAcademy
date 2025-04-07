8d853cf134c7a83ecf1842fee2f1082d397620a7
Gustavo Daniel <gugadanielalvez@gmail.com>
2025-04-07 15:34:48 -0300

    concluído o desafio 5

--- dd1/d5.dart
+++ dd1/d5.dart
@@ -1,3 +1,29 @@
-void main() {
+import 'dart:math';
 
+void main() {
+  final List<String> nomes = [
+    'Ana', 'Francisco', 'Joao', 'Pedro', 'Gabriel', 'Rafaela', 'Marcio',
+    'Jose', 'Carlos', 'Patricia', 'Helena', 'Camila', 'Mateus', 'Gabriel',
+    'Maria', 'Samuel', 'Karina', 'Antonio', 'Daniel', 'Joel', 'Cristiana',
+    'Sebastião', 'Paula'
+  ];
+
+  final List<String> sobrenomes = [
+    'Silva', 'Ferreira', 'Almeida', 'Azevedo', 'Braga', 'Barros', 'Campos',
+    'Cardoso', 'Teixeira', 'Costa', 'Santos', 'Rodrigues', 'Souza', 'Alves',
+    'Pereira', 'Lima', 'Gomes', 'Ribeiro', 'Carvalho', 'Lopes', 'Barbosa'
+  ];
+
+  final nome = sugerirNomeAleatorio(nomes);
+  final sobreNome = sugerirNomeAleatorio(sobrenomes);
+
+  final nomeCompleto = nome + ' ' + sobreNome;
+
+  print('Nome: $nomeCompleto');
+
+}
+
+String sugerirNomeAleatorio(List<String> lista) {
+  final random = Random();
+  return lista[random.nextInt(lista.length)];
 }
