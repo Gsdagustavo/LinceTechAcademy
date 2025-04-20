bbcca68c94b0a53909297ab587eb7d57197acc4b
Gsdagustavo <gugadanielalvez@gmail.com>
2025-04-20 15:48:56 -0300

    adicionados os arquivos das atividades de flutter + resolvida apf1 - ap1

---
+++ Flutter/atividades_flutter/lib/apf1_funcoes/ap1.dart
@@ -0,0 +1,75 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/// Define uma lista de cores
+final colorsList = [
+  Colors.blue,
+  Colors.amber,
+  Colors.brown,
+  Colors.green,
+  Colors.cyan,
+  Colors.indigo,
+  Colors.deepPurple,
+];
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ap1',
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          title: const Text('ap1', style: TextStyle(color: Colors.white)),
+        ),
+
+        body: Center(child: ColorfulButton()),
+      ),
+    );
+  }
+}
+
+class ColorfulButton extends StatefulWidget {
+  const ColorfulButton({super.key});
+
+  @override
+  State<ColorfulButton> createState() => _ColorfulButtonState();
+}
+
+class _ColorfulButtonState extends State<ColorfulButton> {
+  late final VoidCallback _onPressed;
+
+  final random = Random();
+
+  late Color _color = colorsList[random.nextInt(colorsList.length - 1)];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _onPressed = () {
+      setState(() {
+        _color = colorsList[random.nextInt(colorsList.length - 1)];
+        print(_color);
+      });
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: _onPressed,
+      child: Text('Sortear cor', style: TextStyle(color: _color),),
+    );
+  }
+}
